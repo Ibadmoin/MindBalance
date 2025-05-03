b7345641fb4c871e14b77c84839bf939c6c6a8cd
IbadMoin <ibadmoin@gmail.com>
2025-05-03 10:52:09 +0500

    navbar fixes

--- src/components/ui/navbar-menu.jsx
+++ src/components/ui/navbar-menu.jsx
@@ -23,7 +23,7 @@ export const MenuItem = ({
     <div onMouseEnter={() => setActive(item)} className="relative ">
       <motion.p
         transition={{ duration: 0.3 }}
-        className="cursor-pointer text-black hover:opacity-[0.9] dark:text-white">
+        className="cursor-pointer text-black hover:text-indigo-500 dark:text-white">
         {item}
       </motion.p>
       {active !== null && (
@@ -38,11 +38,11 @@ export const MenuItem = ({
                 transition={transition}
                 // layoutId ensures smooth animation
                 layoutId="active"
-                className="bg-white dark:bg-black backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl">
+                className="bg-white  dark:bg-[rgba(0,0,0,0.6)] backdrop-blur-sm rounded-2xl overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl">
                 <motion.div
                   // layout ensures smooth animation
                   layout
-                  className="w-max h-full p-4">
+                  className="w-max h-full p-4 ">
                   {children}
                 </motion.div>
               </motion.div>
@@ -62,7 +62,7 @@ export const Menu = ({
     <nav
       // resets the state
       onMouseLeave={() => setActive(null)}
-      className="relative rounded-full border border-transparent dark:bg-black dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 ">
+      className="relative rounded-full border border-transparent  dark:bg-[rgba(0,0,0,0.6)]  dark:border-white/[0.2] bg-white shadow-input flex justify-center space-x-4 px-8 py-6 ">
       {children}
     </nav>
   );
@@ -75,22 +75,24 @@ export const ProductItem = ({
   src
 }) => {
   return (
-    <a href={href} className="flex space-x-2">
-      <img
-        src={src}
-        width={140}
-        height={70}
-        alt={title}
-        className="shrink-0 rounded-md shadow-2xl" />
-      <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
-      </div>
-    </a>
+<a href={href} className="flex space-x-2 group">
+  <img
+    src={src}
+    width={140}
+    height={70}
+    alt={title}
+    className="shrink-0 rounded-md shadow-2xl"
+  />
+  <div>
+    <h4 className="text-xl font-bold mb-1 text-black dark:text-white group-hover:text-indigo-500">
+      {title}
+    </h4>
+    <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300 group-hover:text-indigo-400">
+      {description}
+    </p>
+  </div>
+</a>
+
   );
 };
 
@@ -101,7 +103,7 @@ export const HoveredLink = ({
   return (
     <a
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black ">
+      className="text-neutral-700 dark:text-neutral-200 hover:text-indigo-400 ">
       {children}
     </a>
   );

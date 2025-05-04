7ef2f8e36bc43d480f840ec242262399b2c8abda
IbadMoin <ibadmoin@gmail.com>
2025-05-04 16:12:26 +0500

    .

--- src/data/featureData.js
+++ src/data/featureData.js
@@ -15,17 +15,18 @@ const featureData = [
       imageUrl: "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2560&auto=format&fit=crop",
       tryNowLink: "/journal",
     },
-    {
-      title: "Secure by Design",
-      subtext: "Hover to see our approach to keeping your data private.",
-      imageUrl: "https://res.cloudinary.com/dfhvlndon/image/upload/v1746356986/ye2nreez0qowf0a0m5qm.png",
-      tryNowLink: "/",
-    },
+  
     {
       title: "Analytics Dash",
       subtext: "Hover to uncover your mood trends and daily stats.",
       imageUrl: "https://res.cloudinary.com/dfhvlndon/image/upload/v1746356887/kb5kiopxqf4cgazf1ujk.png",
       tryNowLink: "/dashboard",
+    },
+    {
+      title: "Secure by Design",
+      subtext: "Hover to see our approach to keeping your data private.",
+      imageUrl: "https://res.cloudinary.com/dfhvlndon/image/upload/v1746356986/ye2nreez0qowf0a0m5qm.png",
+      tryNowLink: "/",
     },
   ];
   
